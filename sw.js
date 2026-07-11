2b14e7fa8562fd801ad3dd33aff7395b43908c27
MkhalidAkbar <122773556+MkhalidAkbar@users.noreply.github.com>
2026-07-11 16:15:41 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='repairlog-v2.6';
+const CACHE='repairlog-v2.7.2';
 const ASSETS=['./','./index.html','./config.js','./icon.png',
   'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2',
   'https://cdn.jsdelivr.net/npm/chart.js@4',
